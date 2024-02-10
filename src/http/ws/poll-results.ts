b34e2e05b6498d266bf97d212a9962fddd0de4bf
Pedro <p20142004@gmail.com>
2024-02-10 14:25:31 -0300

    removendo comentarios e adicionando .env.example

--- src/http/ws/poll-results.ts
+++ src/http/ws/poll-results.ts
@@ -5,17 +5,6 @@ import { voting } from "../../utis/voting-pub-sub";
 export async function pollResults(app: FastifyInstance) {
     app.get("/polls/:pollId/results", { websocket: true }, (connection, request) => {
 
-        // connection.socket.on('message', (message: string) => {
-        //     // message.toString() === 'hi from client'
-        //     // connection.socket.send('you send: ' + message)
-
-        //     // setInterval(() => {
-        //     //     connection.socket.send('server you send message')
-
-        //     // }, 1000)
-          
-        // })
-
         const { params } = request
         const paramsSchema = zod.object({
             pollId: zod.string().uuid()
